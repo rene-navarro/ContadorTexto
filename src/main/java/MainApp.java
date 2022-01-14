61b337f432537cfacaab061a7da620a59180e668
René Navarro <rene.navarro@gmail.com>
2022-01-14 11:49:47 -0700

    Proyecto base.

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MainApp {
+
+    public static void main(String args[]) {
+
+        if (args.length == 0) {
+            System.out.println("Falta el nombre del archivo");
+            System.exit(1);
+        }
+
+        String fileName = args[0];
+
+        FileReader fileReader = null;
+
+        try {
+            fileReader = new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("El nombre del archivo no se encuentra");
+            System.exit(2);
+        }
+
+        BufferedReader in = new BufferedReader(fileReader);
+
+        String textLine = null;
+        int contador = 0;
+        while (true) {
+            try {
+                if (!((textLine = in.readLine()) != null))
+                    break;
+            } catch (IOException e) {
+                System.out.println("Error al leer el archivo de entrada");
+                System.exit(3);
+            }
+            contador++;
+        }
+
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("El archivo " + fileName + " tiene " + contador + " lineas");
+    }
+
+}
